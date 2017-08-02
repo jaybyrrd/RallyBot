09da4758e8214036ce1fb81c87426fa882de1be8
Gaetan Leandre <gaetan.leandre@epitech.eu>
2017-08-02 05:18:09 -0400

    correctifs

--- requests/getCard.js
+++ requests/getCard.js
@@ -5,7 +5,7 @@
 // Login   <gaetan.leandre@epitech.eu>
 //
 // Started on  Tue Aug  1 04:58:19 2017 Gaëtan Léandre
-// Last update Wed Aug  2 05:17:32 2017 Gaëtan Léandre
+// Last update Wed Aug  2 05:18:05 2017 Gaëtan Léandre
 //
 
 var game = require('../schemas/game.js');
@@ -35,7 +35,7 @@ exports.getCard = function(facebookId, gameId, callback)
                                 vote.find({'user': peoples[0]._id}, function(err,votes) {
                                     var done = votes.map(function(el) { return el.card._id } );
                                     card.find({'_id' :{ $in : games[0].cards}, '_id' : {$nin : done}}, function(err, cards) {
-                                        console.log(votes.length);
+                                        console.log(done);
                                         if (cards.length > 0)
                                         {
                                             yelpManager.getInfoYelp(cards[0].yelpId).then(function(resto)
