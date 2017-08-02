6eeab1bc18ff9537cdfd9c516b8a88de40a1ff37
Gaetan Leandre <gaetan.leandre@epitech.eu>
2017-08-02 06:51:21 -0400

    results

--- requests/getCard.js
+++ requests/getCard.js
@@ -5,7 +5,7 @@
 // Login   <gaetan.leandre@epitech.eu>
 //
 // Started on  Tue Aug  1 04:58:19 2017 Gaëtan Léandre
-// Last update Wed Aug  2 05:48:00 2017 Gaëtan Léandre
+// Last update Wed Aug  2 06:51:16 2017 Gaëtan Léandre
 //
 
 var game = require('../schemas/game.js');
@@ -87,11 +87,13 @@ exports.getCard = function(facebookId, gameId, callback)
                                                     }
                                                 });
                                             }).fail(function() {
+                                                console.log("a");
                                                 callback(404, {set_attributes: {cb_yelpId: '-1'}});
                                             });
                                         }
                                         else
                                         {
+                                            console.log("b");
                                             callback(404, {set_attributes: {cb_yelpId: '-1'}});
 					                        return;
                                             //NO OLD CARDS -> add some?
